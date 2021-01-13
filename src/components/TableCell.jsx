1c67638655bc156580d504bd084f249885fa2c9d
Gezim Hakramaj <gezimhakramaj@gmail.com>
2021-01-13 18:30:48 -0500

    Add table-cell base

---
+++ src/components/TableCell.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import './App.css';
+
+class TableCell extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="cell"/>
+        );
+    }
+}
